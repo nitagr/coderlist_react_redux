813f7f7dd5faf30632811fb5c87e3d0064210e8e
Nitish Agrahari <nitish.agrahari@eazydiner.com>
2021-03-10 03:04:52 +0530

    feat: routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import AddForm from './components/AddForm';
+import UserList from './components/UserList';
+import './App.css';
+import Navbar from './components/Navbar';
+import { UserState } from './API';
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
-      <AddForm />
-    </div>
+    <Router>
+      <div >
+        <Navbar />
+
+        <Switch>
+
+          <Route exact path="/">
+            <AddForm />
+          </Route>
+
+          <Route path="/users/create">
+            <AddForm />
+          </Route>
+          
+          <Route path="/users/view">
+            <UserList />
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+
